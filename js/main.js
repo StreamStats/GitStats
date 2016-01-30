e950084e93153f68b5171d9252fa570ffae5015f
TheBaconSpace <bacon@thespacearmy.com>
2016-01-29 22:35:42 -0500

    Updated Stuff

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(event){
+   $('#submit').click(function(event){
+      getData();
+      
+   });
+   $('#formThing').submit(function(event){
+      event.preventDefault();
+      getData();
+   });
+});
+function getData(){
+    $.get('https://api.github.com/users' + $('#inputText').val(), "", function(data){
+         var Avatar = "https://api.github.com/users/" + data['user']['id'] + "/avatar";
+         var Username = data['user']['username'];
+         var url = data['url'];
+         var lastPlayed = "None";
+         var joined = data['createdAt'];
+         var html = '<center><img src="' + Avatar + '"width="100px" height="100px" style="border:3px solid #fff">';
+         html += '<h1>' + Username + '</h1>';
+         html += '<br><b>url: </b>' + url;
+      });;
+}
