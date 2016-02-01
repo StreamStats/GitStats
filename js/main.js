beb00915b0b13678667437994041d82d33b444c4
Bacon_Space <thespacearmy@gmail.com>
2016-02-01 18:14:57 +0000

    Updated Stuff

--- js/main.js
+++ js/main.js
@@ -12,14 +12,16 @@ function getData(){
     $.get('https://api.github.com/users/' + $('#inputText').val(), "", function(data){
          var Avatar = data['avatar_url'];
          var Username = data['login'];
-         var url = data['html_url'];
+         var Location = data['location'];
+         var Url = data['html_url'];
          var lastPlayed = "None";
          var joined = data['created_at'];
          var html = '<center><img src="' + Avatar + '"width="100px" height="100px" style="border:3px solid #fff">';
          html += '<br>Name: ' + Username + '</br>';
-         html += '<br>Joined Github: ' + joined + '</br>';
+         html += '<br>Location: ' + Location + '</br>';
+          html += '<br>Joined Github: ' + joined + '</br>';
          // Center
-         html += '<br><b>Url Link: </b>' + url;
+         html += '<br><b>Url Link: </b>' + Url;
          $('.profile').html(html);
       }).fail(function(data){
             html = '<h1>A Github user with that name does not exist.';
