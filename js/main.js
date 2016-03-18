1a642c0f09aeedbd0b10a7c9ee660ff28c75c527
Bacon_Space <thestreamstats@gmail.com>
2016-03-18 17:19:25 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,9 +19,9 @@ function getData(){
          var joined = data['created_at'];
          var html = '<center><img src="' + Avatar + '"width="100px" height="100px" style="border:3px solid #fff">';
          html += '<h1><span class="label label-success">' + Username + '</h1>';
-         html += '<br><b><span class="label label-primary"style="color: black;">Followers: ' + Followers +'</b>';
-         html += '<br><b><span class="label label-primary"style="color: black;">Repositorys: ' + Repository +'</b>';
-         html += '<br><b><span class="label label-primary"style="color: black;">Location: ' + Location +'</b>';
+         html += '<br><b><span class="label label-primary"style="color: white;">Followers: ' + Followers +'</b>';
+         html += '<br><b><span class="label label-primary"style="color: white;">Repositorys: ' + Repository +'</b>';
+         html += '<br><b><span class="label label-primary"style="color: white;">Location: ' + Location +'</b>';
          html += '<br><b><span class="label label-primary"style="color: black;">Joined: ' + joined +'</b>';
          // Center
                    html += '<br><b><span class="label label-primary"><font size="3" color="white">Url Link: <a href="https://github.com/' + Username + '">GitHub.com/'+ Username +'</a></b>';
