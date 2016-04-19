7181368f4c5376f72e0c0883bdce1866f95de196
Bacon_Space <thestreamstats@gmail.com>
2016-04-19 10:02:12 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,15 +9,15 @@ $(document).ready(function(event){
    });
 });
 function getData(){
-    $.get('https://api.github.com/users/' + $('#inputText').val(), "", function(data){
-         var Avatar = data['avatar_url'];
-         var Username = data['login'];
-         var Location = data['location'];
-         var Repository = data['public_repos'];
-         var Company = data['company'];
-         var Followers = data['followers'];
-         var Url = data['html_url'];
-         var joined = data['created_at'];
+    $.getJSON('https://api.github.com/users/' + $('#inputText').val(),  function(data){
+         var Avatar = data.avatar_url;
+         var Username = data.login;
+         var Location = data.location;
+         var Repository = data.public_repos;
+         var Company = data.company;
+         var Followers = data.followers;
+         var Url = data.html_url;
+         var joined = data.created_at;
          var html = '<center><img src="' + Avatar + '"width="100px" height="100px" style="border:3px solid #fff">';
          html += '<h1><span class="label label-success">' + Username + '</h1>';
          html += '<br><b><span class="label label-primary"style="color: white;">Followers: ' + Followers +'</b>';
